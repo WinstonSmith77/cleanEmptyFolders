68ac9fc92625e2054f248ab96d131a85d79b36b7
Matthias Henning <m.henning@tech2go.de>
2016-07-19 13:05:56 +0200

    Fertig!

--- cleanEmptyFolders/Program.cs
+++ cleanEmptyFolders/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace cleanEmptyFolders
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            var allDirectories = Directory.GetDirectories(args[0], "*", SearchOption.AllDirectories);
+
+            var toDelete = allDirectories.Where(IsEmpty).ToList();
+
+            foreach (var folder in toDelete)
+            {
+                if (Directory.Exists(folder))
+                {
+                    Directory.Delete(folder, true);
+                }
+            }
+        }
+
+        private static bool IsEmpty(string item)
+        {
+            var allFiles = Directory.GetFiles(item, "*", SearchOption.AllDirectories);
+
+            return !allFiles.Any();
         }
     }
 }
